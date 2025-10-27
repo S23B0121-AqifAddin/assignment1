278a7b2ca942fec0e95216f5b1201f7c08721842
S23B0121-AqifAddin <s23b0121@siswa.umk.edu.my>
2025-10-28 03:32:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ st.set_page_config(
     page_title="Student Survey"
 )
 
-visualise = st.Page('design.py', title='Pencapaian Akademik Pelajar', icon=":material/school:")
+visualise = st.Page('home1.py', title='Pencapaian Akademik Pelajar', icon=":material/school:")
 
 home = st.Page('home.py', title='Homepage', default=True, icon=":material/home:")
+
+home = st.Page('page1.py', title='Page 1', default=True, icon=":material/home:")
+
+home = st.Page('page2.py', title='Page 2', default=True, icon=":material/home:")
+
+home = st.Page('page3.py', title='Page 3', default=True, icon=":material/home:")
 
 pg = st.navigation(
         {
