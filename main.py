55e711041a7cf91f60a0d6fc6bb42520e415c641
S23B0121-AqifAddin <s23b0121@siswa.umk.edu.my>
2025-10-28 03:34:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ visualise = st.Page('home1.py', title='Pencapaian Akademik Pelajar', icon=":mate
 
 home = st.Page('home.py', title='Homepage', default=True, icon=":material/home:")
 
-Page1 = st.Page('page1.py', title='Page 1', default=True, icon=":material/home:")
+page_1 = st.Page('page1.py', title='Page 1', default=True, icon=":material/home:")
 
-Page2 = st.Page('page2.py', title='Page 2', default=True, icon=":material/home:")
+page_2 = st.Page('page2.py', title='Page 2', default=True, icon=":material/home:")
 
-Page3 = st.Page('page3.py', title='Page 3', default=True, icon=":material/home:")
+page_3 = st.Page('page3.py', title='Page 3', default=True, icon=":material/home:")
 
 pg = st.navigation(
         {
