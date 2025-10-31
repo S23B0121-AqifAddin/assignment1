add0a528e4b93e217bdf0237a732b5c296af6feb
S23B0121-AqifAddin <s23b0121@siswa.umk.edu.my>
2025-11-01 02:34:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import streamlit as st
 st.set_page_config(page_title="Bangladesh Motorbike Accident", page_icon=":material/school:")
 
 # Define each page
-home = st.Page("home.py", title="Homepage", icon=":material/home:", default=True)
 page_1 = st.Page("page1.py", title="Page 1", icon=":material/description:")
 page_2 = st.Page("page2.py", title="Page 2", icon=":material/bar_chart:")
 page_3 = st.Page("page3.py", title="Page 3", icon=":material/settings:")
