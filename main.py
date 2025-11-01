99abb13092bd11976d9515cfcde7d589ca79e295
S23B0121-AqifAddin <s23b0121@siswa.umk.edu.my>
2025-11-01 21:49:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import streamlit as st
 st.set_page_config(page_title="Bangladesh Motorbike Accident", page_icon=":motorbike:")
 
 # Define each page
-page_1 = st.Page("page1.py", title="Page 1", icon=":material/description:")
-page_2 = st.Page("page2.py", title="Page 2", icon=":material/bar_chart:")
-page_3 = st.Page("page3.py", title="Page 3", icon=":material/settings:")
+page_1 = st.Page("page1.py", title="Page 1")
+page_2 = st.Page("page2.py", title="Page 2")
+page_3 = st.Page("page3.py", title="Page 3")
 
 # Create navigation menu
 pg = st.navigation(
